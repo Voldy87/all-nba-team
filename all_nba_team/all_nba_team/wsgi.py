e07ca7226625e539d21fd027c2d75f6160fe45a0
Andrea <ing.orlandi.andrea@gmail.com>
2018-04-23 17:08:34 +0200

    init django

--- all_nba_team/all_nba_team/wsgi.py
+++ all_nba_team/all_nba_team/wsgi.py
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise#added
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "all_nba_team.settings")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)#added
