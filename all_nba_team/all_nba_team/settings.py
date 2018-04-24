930348fe40874dcf5c5e87a69f07fd68f7294e00
Andrea <ing.orlandi.andrea@gmail.com>
2018-04-24 10:31:31 +0200

    test django, try 1

--- all_nba_team/all_nba_team/settings.py
+++ all_nba_team/all_nba_team/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'luumx&rx58hg68z6dg$915k&$tt)p7_n-p&fj%xgexa$zay7vz'
+SECRET_KEY = '_o8=a90k47&xtfn#m5)qgcoe9jbpq6k#9rtg&3^_p6=!-@(4t!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -118,18 +118,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-
-#commented line above and added all the bunch below
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-#STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
-
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
