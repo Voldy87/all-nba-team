515c77a9c638f027b81ac286341eedee790098ef
Andrea <ing.orlandi.andrea@gmail.com>
2018-04-30 17:12:51 +0200

    complete the landing page

---
+++ all_nba_team/splashome/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render,render_to_response
+
+# Create your views here.
+
+from django.http import HttpResponse
+
+
+def index(request):
+    #return HttpResponse("Hello, world. You're at the polls index.")
+    return render(
+        request,
+        'index.html',
+        {}#context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
+    )
