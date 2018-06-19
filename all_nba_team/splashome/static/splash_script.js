562b34f8d827bfc5254bc535a67b7e7e97858c34
Andrea <ing.orlandi.andrea@gmail.com>
2018-06-19 17:03:35 +0200

    add slideshow draft

---
+++ all_nba_team/splashome/static/splash_script.js
@@ -0,0 +1,16 @@
+setTimeout(function(){
+    document.body.classList.add("bg");
+    document.body.style.cursor = "initial";
+    document.querySelector(".title-container").style.overflow = 'visible';
+    document.querySelector(".title-container").style.top = '5vh';
+    document.querySelector("h1").innerHTML = 'All-NBA Teams';
+    document.querySelector(".subtitle").style.display = 'initial';
+    document.querySelector("h1").style.fontStyle = 'normal';
+}, 2000)
+setTimeout(function(){
+    document.querySelector("h1").classList.add("title");
+    document.querySelector(".container_author").style.display = 'flex';
+    document.querySelector("button").style.display = 'initial';
+    document.querySelector("svg.corner").style.display = 'initial';
+    document.querySelector("#tracker").classList.add("dis");
+}, 3800)
