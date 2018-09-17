7631dfddc958caba6412dfaa7e232ca271def66b
Andrea <ing.orlandi.andrea@gmail.com>
2018-09-17 12:53:35 +0200

    not allow to see the databases credentials

--- all_nba_team/all_nba_team/settings.py
+++ all_nba_team/all_nba_team/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
-import os
+import os, configparser
 import django_heroku
 
 from django.utils.translation import gettext_lazy as _
@@ -18,12 +18,19 @@ from django.utils.translation import gettext_lazy as _
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+#read config gile (private)
+config = configparser.ConfigParser()
+path = os.path.join(os.path.dirname(__file__), '../../config.ini')
+print(path)
+config.read(path)
+print(config.sections())
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_o8=a90k47&xtfn#m5)qgcoe9jbpq6k#9rtg&3^_p6=!-@(4t!'
+SECRET_KEY = config['PROD']['SecretKey']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -95,22 +102,22 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'd4s8j5s205po7t',
         'USER': 'zyqgdtpsbttnxn',
-        'PASSWORD': '28e93dff8395329a9c0760a9ad084bae4e028646af7ad5eec2b24c53f9f65046',
-        'HOST': 'ec2-54-227-250-33.compute-1.amazonaws.com',
+        'PASSWORD': config['PROD']['DbDefaultPass'],
+        'HOST': config['PROD']['DbDefaultHost'],
         'PORT': '5432',
     },
     'data': { # private do server (allnba data)
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'all_nba_team',
         'USER': 'postgres',
-        'PASSWORD': 'massaciuccoli',
-        'HOST': '46.101.238.71',
+        'PASSWORD': config['PROD']['DbDataPass'],
+        'HOST': config['PROD']['DbDataHost'],
         'PORT': '5432',
     },
     'redis': { # private do server (allnba data)
         'NAME': 'allnbateam',
-        'PASSWORD': '9jQ4vl5SZWYbPdOJYejag5gzwOHjeF8U',
-        'HOST': 'redis-13191.c55.eu-central-1-1.ec2.cloud.redislabs.com',
+        'PASSWORD': config['PROD']['DbRedisPass'],
+        'HOST': config['PROD']['DbRedisHost'],
         'PORT': '13191',
     },
 }
